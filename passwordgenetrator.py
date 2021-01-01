f1d954336b466cbc5a88c67431417545e0ac1fe8
Eshan Singh <32596297+R0X4R@users.noreply.github.com>
2021-01-01 14:52:36 +0530

    Password Generator

---
+++ passwordgenetrator.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# By Eshan Singh - twiiter.com/R0X4R
+
+# modules
+import random
+
+# declaring variables
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase_letters = uppercase_letters.lower()
+digits = "0123456789"
+symbols = "()_+=-*&^%$#@!~`:|?.,{}[]/"
+
+# setting boolean values
+uppercase, lowercase, number, symbol = True, True, True, True,
+
+all = ""
+
+# setting password gen conditions
+if uppercase:
+    all += uppercase_letters
+if lowercase:
+    all += lowercase_letters
+if number:
+    all += digits
+if symbol:
+    all += symbols
+
+# lenght and amount of passwords we get
+length = 20
+amount = 10
+
+'''
+To generate the same password use
+seed = "eshan" 
+random.seed(seed)
+'''
+
+# generating passwords
+for x in range(amount):
+    password = "".join(random.sample(all, length))
+    print("Your password: " + password)
